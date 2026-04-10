7e8610b664aab9a8b74929b3578c2ab64ae03578
tote10 <motialemu9@gmail.com>
2026-04-10 10:29:11 +0300

    feat: add database configuration and PDO connection script

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = "127.0.0.1";
+$dbname = "driver_license_school_db";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username,$password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
